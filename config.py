1bd87e2d37b7d25953afcf5fda0fb1507a267a2a
jd283bb-chambal <jd283.bb@gmail.com>
2025-10-22 05:03:23 +0530

    Update and rename data.json to config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+import re
+
+# ======================= API & Bot Config =======================
+API_ID = 26250263
+API_HASH = "24b066ce7a9020dfbd69b4dc593993f1"
+BOT_TOKEN = "7981496411:AAHSjcC62nEmpkA2xXMUT4Tl1X3_9xFtZDE"
+ADD_TO_GROUP_USERNAME = "shieldprotector_bot"
+SUPPORT_LINK = "https://t.me/+yGiJaSdHDoRlN2Zl"
+SESSION_FILE = "ShieldX.session"
+
+# MongoDB connection URI
+MONGO_URI = "your_mongodb_url"
+
+DEFAULT_WARNING_LIMIT = 3
+DEFAULT_PUNISHMENT = "mute" # Options: "mute", "ban"
+DEFAULT_CONFIG = ("warn", DEFAULT_WARNING_LIMIT, DEFAULT_PUNISHMENT)
+
+# Regex pattern to detect URLs in user bios
+URL_PATTERN = re.compile(
+    r'(https?://|www\.)[a-zA-Z0-9.\-]+(\.[a-zA-Z]{2,})+(/[a-zA-Z0-9._%+-]*)*' #done change here
+)
