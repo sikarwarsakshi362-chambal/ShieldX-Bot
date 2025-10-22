e72af985637f24188b138beedbd08f4ea1eb2a9d
jd283bb-chambal <jd283.bb@gmail.com>
2025-10-22 05:47:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import re
+import os
+import motor.motor_asyncio
 
 # ======================= API & Bot Config =======================
 API_ID = 26250263
@@ -7,19 +9,23 @@ BOT_TOKEN = "7981496411:AAHSjcC62nEmpkA2xXMUT4Tl1X3_9xFtZDE"
 ADD_TO_GROUP_USERNAME = "shieldprotector_bot"
 SUPPORT_LINK = "https://t.me/+yGiJaSdHDoRlN2Zl"
 SESSION_FILE = "ShieldX.session"
-import os
-import motor.motor_asyncio
-# MongoDB connection URI
-mongo_url = os.environ.get("MONGO_URI")
-client = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
-db = client["mydb"]
-print("✅ Connected to MongoDB"
 
+# ======================= MongoDB Config =======================
+mongo_url = os.environ.get("MONGO_URI")
+
+try:
+    client = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
+    db = client["mydb"]
+    print("✅ Connected to MongoDB")
+except Exception as e:
+    print(f"❌ MongoDB Connection Error: {e}")
+
+# ======================= Default Config =======================
 DEFAULT_WARNING_LIMIT = 3
-DEFAULT_PUNISHMENT = "mute" # Options: "mute", "ban"
+DEFAULT_PUNISHMENT = "mute"  # Options: "mute", "ban"
 DEFAULT_CONFIG = ("warn", DEFAULT_WARNING_LIMIT, DEFAULT_PUNISHMENT)
 
-# Regex pattern to detect URLs in user bios
+# ======================= Regex Pattern =======================
 URL_PATTERN = re.compile(
-    r'(https?://|www\.)[a-zA-Z0-9.\-]+(\.[a-zA-Z]{2,})+(/[a-zA-Z0-9._%+-]*)*' #done change here
+    r'(https?://|www\.)[a-zA-Z0-9.\-]+(\.[a-zA-Z]{2,})+(/[a-zA-Z0-9._%+-]*)*'
 )
